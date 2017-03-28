5eb98d2231b0dcfd8203fbb3f1d451d9040a30fd
fuhongjuan <fuhongjuan@meituan.com>
2017-03-28 17:44:03 +0800

    增加可同时显示html,css,result任意两项

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function codepenTagRender(args) {
   const defaultTab = args[3] != null ? args[3] : 'result';
   const height = args[4] != null ? args[4] : 300;
   const width = args[5] != null ? args[5] : '100%';
-  const src = `//codepen.io/${userId}/embed/${slugHash}?height=${height}&theme-id=${theme}&slug-hash=${slugHash}&default-tab=${defaultTab}`;
+  const src = `//codepen.io/${userId}/embed/${slugHash}?height=${height}&theme-id=${theme}&slug-hash=${slugHash}&default-tab=${defaultTab}&embed-version=2`;
   const id = `cp_embed_${slugHash.replace(/\//g, '_')}`
 
   return `<iframe id=${id} src=${src} scrolling="no" frameborder="no" height="${height}" allowTransparency="true" allowfullscreen="true" class="cp_embed_iframe" style="width: ${width}; overflow: hidden;"></iframe>`;
